e528d2da606f398d035932d30ea91f87b0545f98
Joel Young <949135973@qq.com>
2020-10-10 01:21:03 +0800

    Day 1 over

--- src/main/kotlin/PluginMain.kt
+++ src/main/kotlin/PluginMain.kt
@@ -1,5 +1,6 @@
 package org.Reforward.mirai.plugin
 
+import net.mamoe.mirai.console.extension.PluginComponentStorage
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
 import net.mamoe.mirai.utils.info
@@ -10,7 +11,17 @@ object PluginMain : KotlinPlugin(
         version = "0.0.1"
     )
 ) {
+
     override fun onEnable() {
         logger.info { "Welcome to 权益小窝‘s 转发插件" }
+    }
+
+    override fun onLoad() {
+        super.onLoad()
+        TODO("Not yet implemented")
+    }
+
+    override fun onDisable() {
+        super.onDisable()
     }
 }
