4f31bf083aa905afa8ae88fdf279671f602031c0
BuildTools <unconfigured@null.spigotmc.org>
2020-10-10 21:42:00 +0800

    初步构建

--- src/main/kotlin/PluginMain.kt
+++ src/main/kotlin/PluginMain.kt
@@ -1,25 +1,46 @@
-package org.example.mirai.plugin
+package org.Reforward.mirai.plugin
 
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
 import net.mamoe.mirai.utils.info
 import net.mamoe.mirai.console.data.AutoSavePluginConfig
 import net.mamoe.mirai.console.data.value
+import net.mamoe.mirai.event.subscribeGroupMessages
+import net.mamoe.mirai.message.data.Message
+import net.mamoe.mirai.message.data.MessageChain
+import net.mamoe.mirai.message.data.sendTo
 import org.jetbrains.kotlinx.serialization.compiler.backend.jvm.ARRAY
+
+val PluginID = "org.example.mirai-plugin"
+val PluginVersion = "0.0.1"
 
 object PluginMain : KotlinPlugin(
     JvmPluginDescription(
-        id = "org.example.mirai-plugin",
-        version = "0.1.0"
+        id = PluginID,
+        version = PluginVersion
     )
 ) {
     override fun onEnable() {
-        logger.info { "Writen by QYXW " }
+        logger.info { "Writen by QYXW" }
+        Mydata.reload()
+    }
+
+    private fun ForwardtheMsg() {
+        subscribeGroupMessages {
+            always {
+
+            }
+        }
+    }
+
+    private fun send(group : Group, messagechain : MessageChain, bot : Bot) {
+        val groups = Mydata.groups
+        for (id : Long in groups) {
+
+        }
     }
 }
 
-object Mydata : AutoSavePluginConfig("Empty") {
-    val groups: Array<String> by value(emptyArray<String>())
-
-
+object Mydata : AutoSavePluginConfig("Groups") {
+    val groups: Array<Long> by value(emptyArray<Long>())
 }
